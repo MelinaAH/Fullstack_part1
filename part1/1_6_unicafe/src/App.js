19fb13f929a6a068af99197ef76861e103b64949
Melina <melina.aalto-halme@tuni.fi>
2022-09-29 17:14:19 +0300

    1.6 unicafe step 1 done

---
+++ part1/1_6_unicafe/src/App.js
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+
+const Display = ( { good, neutral, bad }) => {
+  return (
+    <>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </>
+  )
+}
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodClick = () => {
+    setGood(good + 1)
+  }
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const handleBadClick = () => {
+    setBad(bad + 1)
+  }
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={handleGoodClick}>good</button>
+      <button onClick={handleNeutralClick}>neutral</button>
+      <button onClick={handleBadClick}>bad</button>
+      <h1>statistics</h1>
+      <Display good={good} neutral={neutral} bad={bad}></Display>
+    </div>
+  )
+}
+
+export default App
