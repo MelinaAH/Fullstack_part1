6fe3ce311a6c9532f85b47a8bf89945841e18168
Melina <melina.aalto-halme@tuni.fi>
2022-09-29 17:37:12 +0300

    1.7 unicafe step 2 done

--- part1/1_6_unicafe/src/App.js
+++ part1/1_6_unicafe/src/App.js
@@ -1,11 +1,18 @@
 import { useState } from 'react'
 
 const Display = ( { good, neutral, bad }) => {
+  const all = good + neutral + bad
+  const average = (good - bad) / all
+  const positive = good / all * 100
+
   return (
     <>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
     </>
   )
 }
