8ac3d8eb698b398c52ada891cfc3a99029350853
Melina <melina.aalto-halme@tuni.fi>
2022-09-29 17:46:43 +0300

    1.9 unicafe step 4 done

--- part1/1_6_unicafe/src/App.js
+++ part1/1_6_unicafe/src/App.js
@@ -5,6 +5,13 @@ const Statistics = ( { good, neutral, bad }) => {
   const average = (good - bad) / all
   const positive = good / all * 100
 
+  if (good === 0 && neutral === 0 && bad === 0) {
+    return (
+      <div>
+        No feedback given
+      </div>
+    )
+  }
   return (
     <>
       <p>good {good}</p>
