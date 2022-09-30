5778a2a28a618053af248777c0044014186c2c80
Melina <melina.aalto-halme@tuni.fi>
2022-09-30 12:42:06 +0300

    2.5 App.js file and Course.js file

--- part2/2_1_kurssitiedot/src/App.js
+++ part2/2_1_kurssitiedot/src/App.js
@@ -1,42 +1,4 @@
-const Total = ({ parts }) => {
-  const total = parts.reduce((prev, curr) => prev + curr.exercises, 0);
-
-  return (
-    <div>
-      <p><b>total of {total} exercises</b></p>
-    </div>
-  )
-}
-
-const Content = ({ name, parts }) => {
-  return (
-    <div>
-      <h2>{name}</h2>
-      {parts.map(part =>
-        <p key={part.id}>{part.name} {part.exercises}</p>
-      )}
-      <Total parts={parts} />
-    </div>
-  )
-}
-
-const Header = ({ courses }) => {
-  return (
-    <div>
-      {courses.map(course =>
-        <Content key={course.id} name={course.name} parts={course.parts} />
-      )}
-    </div>
-  )
-}
-
-const Course = ({ courses }) => {
-  return (
-    <div>
-      <Header courses={courses} />
-    </div>
-  )
-}
+import Course from "./components/Course"
 
 const App = () => {
   const courses = [
