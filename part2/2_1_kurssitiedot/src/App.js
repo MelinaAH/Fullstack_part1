fd09b080da9874aecba0639b6a368a6577b4e87e
Melina <melina.aalto-halme@tuni.fi>
2022-09-30 11:02:47 +0300

    2.1 App file done

---
+++ part2/2_1_kurssitiedot/src/App.js
@@ -0,0 +1,72 @@
+const Part = ({ parts }) => {
+  console.log('part:', parts);
+
+  return (
+    <div>
+      {parts.map(part => 
+        <p key={part.id}>{part.name} {part.exercises}</p>)}
+    </div>
+  )
+}
+
+const Content = ({ course }) => {
+  console.log('Content:', course.parts);
+
+  return (
+    <div>
+      <Part parts={course.parts} />
+    </div>
+  )
+}
+
+const Header = ({ course }) => {
+  return (
+    <div>
+      <h1>{course.name}</h1>
+    </div>
+  )
+}
+
+const Course = ({ course }) => {
+  console.log('course:', course.name);
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content course={course} />
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    id: 1,
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+  console.log('app toimii:', course.name);
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+export default App
