b5d825f4847048c0cd797b55ced47c87cb172cb9
Melina <melina.aalto-halme@tuni.fi>
2022-10-25 13:50:51 +0300

    2.9. puhelinluettelo step4 done

--- part2/2_6_puhelinluettelo/src/App.js
+++ part2/2_6_puhelinluettelo/src/App.js
@@ -2,15 +2,19 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [newName, setNewName] = useState('')
-  const [phoneNumber, setPhoneNumber] = useState('')
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
+  const [newName, setNewName] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [searchCriterion, setSearchCriterion] = useState('');
+  const [showAll, setShowAll] = useState(true);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    
-    if (persons.some(({name}) => name === newName)) {
+    if (persons.some(({ name }) => name === newName)) {
       console.log('Name already exists');
       alert(`${newName} is already added to phonebook`);
     }
@@ -36,9 +40,29 @@ const App = () => {
     setPhoneNumber(e.target.value);
   }
 
+  const handleSearch = (e) => {
+    console.log('name to search:', e.target.value);
+    setSearchCriterion(e.target.value);
+    setShowAll(false);
+    if (e.target.value === '') {
+      setShowAll(true);
+    }
+  }
+
+  const namesToShow = showAll
+    ? persons
+    : persons.filter(person =>
+      person.name.toLowerCase() === searchCriterion.toLowerCase());
+
+  console.log('names to show:', namesToShow);
+
   return (
     <div>
       <h2>Phonebook</h2>
+        <div>
+          filter shown with <input value={searchCriterion} onChange={handleSearch} />
+        </div>
+      <h2>add a new</h2>
       <form onSubmit={handleSubmit}>
         <div>
           name: <input value={newName} onChange={handleNameFiled} />
@@ -51,12 +75,12 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => 
-        <p key={person.name}>{person.name} {person.number}</p>
-      )}
+      {
+        namesToShow.map(name =>
+          <p key={name.name}>{name.name} {name.number}</p>)
+      }
     </div>
   )
-
 }
 
 export default App
